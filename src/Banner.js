7978d668ecb776d404ab9c003a8637f8b72a9126
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-08-09 13:34:50 +0500

    Implementing API and Adding Row files

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Banner.css"
-
+import axios from "./axios"
+import requests from "./Requests"
 function Banner() {
+       const [movie,  setMovie] = useState([]);
+
+       useEffect (() => {
+          async function fetchData() {
+              const request = await axios.get(requests.fetchNetflixOriginals);
+              setMovie(
+                  request.data.results[
+                      Math.floor(Math.random() * request.data.results.length - 1)
+                  ]
+              );
+              return request;
+          }
+          fetchData()
+       }, []);
+
+       console.log(movie);
+
 
        function truncate(string, n) {
           return string?.length > n ? string.substr(0, n -1) + '...' : string;
@@ -11,21 +29,21 @@ function Banner() {
     className="banner" 
     style={{
           backgroundSize: "cover",
-          backgroundImage: `url("https://i.pinimg.com/originals/b0/42/8d/b0428dfa06364283df7f951e73411cf5.jpg")`,
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
           backgroundPosition: "center center",
         }}>
           
           <div className="bannerContents">
 
-              <h1 className="bannerTitle"> Movie Name</h1>
+              <h1 className="bannerTitle"> 
+               {movie?.title || movie?.name || movie?.original_name}
+              </h1>
               <div className="bannerButtons">
                   <button className="bannerButton">Play</button>
                   <button className="bannerButton">My List</button>
               </div>
               <h1 className="bannerDescription"> 
-              {truncate (`This is a test description This is a test description 
-              This is a test descriptionThis is a test description This is a test description 
-              This is a test description This is a test description`, 150)}
+              {truncate (movie?.overview, 150)}
               </h1>
           </div>
 
---
+++ src/Row.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Row.css"
+
+function Row({ title, fetchUrl, isLargeRow = false}) {
+    
+    const [ movies]
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+        </div>
+    )
+}
+
+export default Row
